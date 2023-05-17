43d63255611e88c797fdbb247e243999d96e75c4
Pontes <hpo.pontes@gmail.com>
2023-05-17 10:28:08 -0300

    usando o while

--- script.js
+++ script.js
@@ -4,12 +4,10 @@ function tocaSomPom(){
 }
 
 const listaTeclas = document.querySelector('.tecla');
-listaTeclas[0].onclick = tocaSomPom;
-listaTeclas[1].onclick = tocaSomClap;
-listaTeclas[2].onclick = tocaSomTim;
-listaTeclas[3].onclick = tocaSomPuff;
-listaTeclas[4].onclick = tocaSomSplash;
-listaTeclas[5].onclick = tocaSomToim;
-listaTeclas[6].onclick = tocaSomPsh;
-listaTeclas[7].onclick = tocaSomTic;
-listaTeclas[8].onclick = tocaSomPom;
+
+let cont = 0
+//enequanto
+while (cont = listaTeclas.lenght){
+    listaTeclas[0].onclick = tocaSomPom;
+    cont++;
+}
