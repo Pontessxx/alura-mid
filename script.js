cbdae419ddcc1b972dfcf7c852600384ef16fd60
Pontes <hpo.pontes@gmail.com>
2023-05-17 10:25:11 -0300

    testando a listas com uma constante

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+//Pom ----------------------------------------------
 function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
 }
-document.querySelector('.tecla').onclick = tocaSomPom;
+
+const listaTeclas = document.querySelector('.tecla');
+listaTeclas[0].onclick = tocaSomPom;
+listaTeclas[1].onclick = tocaSomClap;
+listaTeclas[2].onclick = tocaSomTim;
+listaTeclas[3].onclick = tocaSomPuff;
+listaTeclas[4].onclick = tocaSomSplash;
+listaTeclas[5].onclick = tocaSomToim;
+listaTeclas[6].onclick = tocaSomPsh;
+listaTeclas[7].onclick = tocaSomTic;
+listaTeclas[8].onclick = tocaSomPom;
