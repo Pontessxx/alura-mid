5901462e79f4dc752ca6eb12de555b52053dfe35
Pontes <hpo.pontes@gmail.com>
2023-05-17 11:04:23 -0300

    codigo repetitivo usando o if

--- script.js
+++ script.js
@@ -16,8 +16,14 @@ for (let contador = 0; contador < listaDeTeclas.length; contador++) {
     }
     
     //adiciona a classe ativa para o botão ao apertar o enter quando selecionada o buttom
-    tecla.onkeydown = function (){
-        tecla.classList.add('ativa')
+    tecla.onkeydown = function (evento){
+        //console.log(evento.code== 'Space')
+        if (evento.code === 'Space'){
+            tecla.classList.add('ativa')
+        }
+        if (evento.code === 'Enter'){
+            tecla.classList.add('ativa')
+        }
     }
     tecla.onkeyup = function (){
         tecla.classList.remove('ativa')
